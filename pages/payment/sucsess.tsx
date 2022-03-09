11ca3fbb14657f282f02da2d5dbc644bfeec5923
Dylan Mashini <Dylanmashini123@gmail.com>
2022-03-09 16:59:49 -0500

    fixed sucsess bug

--- pages/payment/sucsess.tsx
+++ pages/payment/sucsess.tsx
@@ -1,25 +1,27 @@
-import Layout from '../../layouts/Main';
-import {clearCart} from "../../store/actions/cartActions";
-import { useDispatch } from "react-redux"
-import { useEffect } from "react";
+import Layout from "../../layouts/Main";
+import { clearCart } from "../../store/actions/cartActions";
+import { useDispatch } from "react-redux";
+import { useEffect, useCallback } from "react";
 //clear the redux state
-export default function Named()  {
+export default function () {
+	const dispatch = useDispatch();
     useEffect(() => {
-
-    const dispatch = useDispatch();
-    dispatch(clearCart());
-
-})
-   
-    return(
-        <Layout>
-            <section className="error-page">
-            <div className={"container"}>
-                <h1>Sucsessful Checkout</h1>
-                <p>You will recive a confirmation email in your inbox soon</p>
-                <a href={"/"} className={"btn btn--rounded btn--yellow"}>Return Home</a>
-            </div>
-            </section>
-        </Layout>       
-    )
+        dispatch(clearCart());
+    })
+    
+	return (
+		<Layout>
+			<section className="error-page">
+				<div className={"container"}>
+					<h1>Sucsessful Checkout</h1>
+					<p>
+						You will recive a confirmation email in your inbox soon
+					</p>
+					<a href={"/"} className={"btn btn--rounded btn--yellow"}>
+						Return Home
+					</a>
+				</div>
+			</section>
+		</Layout>
+	);
 }
