20fb759b3f537c30d1e584520d927d5869d5c0a8
Dylan Mashini <Dylanmashini123@gmail.com>
2022-03-10 18:47:45 -0500

    fixed bug

--- utils/getAllProds.js
+++ utils/getAllProds.js
@@ -1,124 +1,3 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
-const { default: next } = require("next");
-var csv = require("jquery-csv");
-
-const APICall = async (offset, apiKey, merchantID, url) => {
-	const options = {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${apiKey}`,
-		},
-	};
-	return new Promise((resolve, reject) => {
-		fetch(
-			`${url}/v3/merchants/${merchantID}/items?limit=1000&offset=${offset}`,
-			options
-		)
-			.then(res => res.json())
-			.then(res => {
-				resolve(res.elements);
-			})
-			.catch(err => reject(err));
-	});
-};
-const run = (apiKey, merchantID, url) => {
-	const popularity = csv.toObjects(csvData);
-
-	var allProds = [];
-	var run = true;
-	var count = 0;
-	loop();
-	function loop() {
-		console.log("here");
-
-		APICall(1000 * count, apiKey, merchantID, url)
-			.then(data => {
-				allProds = allProds.concat(data);
-				console.log(allProds.length);
-				if (data.length >= 1000) {
-					count++;
-					loop();
-				} else {
-					const finalProds = [];
-					const finalProdsNoImg = [];
-					allProds.forEach((prod, index) => {
-						//check if sku starts with 0, if so remove the 0
-						if (prod.sku.charAt(0) === "0") {
-							prod["images"] = [
-								"/products/" + prod.sku.substring(1) + ".jpeg",
-							];
-						} else {
-							prod["images"] = [
-								"/products/" + prod.sku + ".jpeg",
-							];
-						}
-
-						prod["reviews"] = [];
-						prod["currentPrice"] = prod.price / 100;
-						if (prod.stockCount > 0) {
-							if (fs.existsSync("public" + prod["images"][0])) {
-								finalProds.push(prod);
-							} else {
-								finalProdsNoImg.push(prod);
-							}
-						}
-					});
-					for (var i = 0; i < finalProdsNoImg.length; i++) {
-						finalProds.push(finalProdsNoImg[i]);
-					}
-					for (var i = 0; i < finalProds.length; i++) {
-						const prod = finalProds[i];
-						const sku = prod.sku;
-						for (var i = 0; i < popularity.length; i++) {
-							if (popularity[i].sku === sku) {
-								i = 9000;
-								finalProds[i]["unitsSold"] =
-									popularity[i]["# of Items Sold"];
-							}
-						}
-					}
-					finalProds.sort((a, b) => {
-						if (a.unitsSold == null) {
-							a.unitsSold = 0;
-						}
-						if (b.unitsSold == null) {
-							b.unitsSold = 0;
-						}
-						return a.unitsSold - b.unitsSold;
-					});
-					fetch("https://dna-nutrition.vercel.app/api/products")
-						.then(res => res.json)
-						.then(res => {
-							if (res == finalProds) {
-								console.log("No updates to inventory");
-								return;
-							}
-						});
-					fs.writeFile(
-						"prods.json",
-						JSON.stringify(finalProds),
-						err => {
-							if (err) {
-								console.log(err);
-							}
-						}
-					);
-				}
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-};
-run(
-	"f4e689f5-197b-3a25-9f99-0024da952d4f",
-	"ZX3HTEDW8YD01",
-	"https://sandbox.dev.clover.com"
-);
-//prod: 5ff7477a-29f9-a092-9522-9478e036b9e8
-//prod: DMF44Z2ZC6PTT
-
 const csvData = `Item SKU,Item ID,,Item Name,,# of Items Sold
 743715000537,DKQDJRT0JRQZ4,,5-HTP 100mg 120ct,,3
 733739030146,AXXFWBYV4S8T8,,7-Keto 100mg 120ct,,1
@@ -871,3 +750,124 @@ c970200c-128f-46fd-8a1c-e64a29e48c1c,WGCZVPP3AJ2NY,,Ultra Stress,,1
 672975315559,3D1SA0QM1R0RP,,ZONE Arnold Palmer 20srv,,1
 672975315849,SVWZK5ZNRZ9J6,,Zone Berrylicious 20 Srv,,1
 672975315566,J7DNM57DGCZ6G,,ZONE Sour Candy 20srv,,1`;
+
+const fetch = require("node-fetch");
+const fs = require("fs");
+const { default: next } = require("next");
+var csv = require("jquery-csv");
+
+const APICall = async (offset, apiKey, merchantID, url) => {
+	const options = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${apiKey}`,
+		},
+	};
+	return new Promise((resolve, reject) => {
+		fetch(
+			`${url}/v3/merchants/${merchantID}/items?limit=1000&offset=${offset}`,
+			options
+		)
+			.then(res => res.json())
+			.then(res => {
+				resolve(res.elements);
+			})
+			.catch(err => reject(err));
+	});
+};
+const run = (apiKey, merchantID, url) => {
+	const popularity = csv.toObjects(csvData);
+
+	var allProds = [];
+	var run = true;
+	var count = 0;
+	loop();
+	function loop() {
+		console.log("here");
+
+		APICall(1000 * count, apiKey, merchantID, url)
+			.then(data => {
+				allProds = allProds.concat(data);
+				console.log(allProds.length);
+				if (data.length >= 1000) {
+					count++;
+					loop();
+				} else {
+					const finalProds = [];
+					const finalProdsNoImg = [];
+					allProds.forEach((prod, index) => {
+						//check if sku starts with 0, if so remove the 0
+						if (prod.sku.charAt(0) === "0") {
+							prod["images"] = [
+								"/products/" + prod.sku.substring(1) + ".jpeg",
+							];
+						} else {
+							prod["images"] = [
+								"/products/" + prod.sku + ".jpeg",
+							];
+						}
+
+						prod["reviews"] = [];
+						prod["currentPrice"] = prod.price / 100;
+						if (prod.stockCount > 0) {
+							if (fs.existsSync("public" + prod["images"][0])) {
+								finalProds.push(prod);
+							} else {
+								finalProdsNoImg.push(prod);
+							}
+						}
+					});
+					for (var i = 0; i < finalProdsNoImg.length; i++) {
+						finalProds.push(finalProdsNoImg[i]);
+					}
+					for (var i = 0; i < finalProds.length; i++) {
+						const prod = finalProds[i];
+						const sku = prod.sku;
+						for (var i = 0; i < popularity.length; i++) {
+							if (popularity[i].sku === sku) {
+								i = 9000;
+								finalProds[i]["unitsSold"] =
+									popularity[i]["# of Items Sold"];
+							}
+						}
+					}
+					finalProds.sort((a, b) => {
+						if (a.unitsSold == null) {
+							a.unitsSold = 0;
+						}
+						if (b.unitsSold == null) {
+							b.unitsSold = 0;
+						}
+						return a.unitsSold - b.unitsSold;
+					});
+					fetch("https://dna-nutrition.vercel.app/api/products")
+						.then(res => res.json)
+						.then(res => {
+							if (res == finalProds) {
+								console.log("No updates to inventory");
+								return;
+							}
+						});
+					fs.writeFile(
+						"prods.json",
+						JSON.stringify(finalProds),
+						err => {
+							if (err) {
+								console.log(err);
+							}
+						}
+					);
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+};
+run(
+	"f4e689f5-197b-3a25-9f99-0024da952d4f",
+	"ZX3HTEDW8YD01",
+	"https://sandbox.dev.clover.com"
+);
+//prod: 5ff7477a-29f9-a092-9522-9478e036b9e8
+//prod: DMF44Z2ZC6PTT
