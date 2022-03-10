eeff35b7db55b9348162af8479114825929f1c72
Dylan Mashini <Dylanmashini123@gmail.com>
2022-03-10 12:28:30 -0500

    added mdx support

--- next.config.js
+++ next.config.js
@@ -19,3 +19,16 @@ module.exports = {
 		includePaths: [path.join(__dirname, "styles")],
 	},
 };
+const withMDX = require("@next/mdx")({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+		// If you use `MDXProvider`, uncomment the following line.
+		// providerImportSource: "@mdx-js/react",
+	},
+});
+module.exports = withMDX({
+	// Append the default value with md extensions
+	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
