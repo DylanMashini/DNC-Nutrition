812e3bca2bfdb871ac663173b6b39c1ba3786d85
DylanMashini <dylanmashini123@gmail.com>
2022-03-12 21:04:46 -0500

    lots of stuff

--- components/breadcrumb/index.js
+++ components/breadcrumb/index.js
@@ -1,13 +1,19 @@
-const Breadcrumb = () => (
-  <section className="breadcrumb">
-    <div className="container">
-      <ul className="breadcrumb-list">
-        <li><a href="#"><i className="icon-home"></i></a></li>
-        <li>All Products</li>
-      </ul>
-    </div>
-  </section>
+import Link from "next/link";
+const Breadcrumb = ({ page = 1 }) => (
+	<section className="breadcrumb">
+		<div className="container">
+			<Link href={`/products/${page}`} id={"clickable"}>
+				<ul className="breadcrumb-list" id={"clickable"}>
+					<li id={"clickable"}>
+						<a id={"clickable"}>
+							<i className="icon-home" id={"clickable"}></i>
+						</a>
+					</li>
+					<li id={"clickable"}>All Products</li>
+				</ul>
+			</Link>
+		</div>
+	</section>
 );
 
-
-export default Breadcrumb
+export default Breadcrumb;
