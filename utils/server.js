3473a03739406958c2e30d285e7aa0bebadbc85e
DylanMashini <dylanmashini123@gmail.com>
2022-04-08 11:54:17 -0400

    fixed url

--- utils/server.js
+++ utils/server.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://dna-nutrition.vercel.app";
+	: "https://dnc-nutrition.vercel.app";
