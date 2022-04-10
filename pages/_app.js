a5030734adfd00510d48fb9cb74c72455dd27147
DylanMashini <dylanmashini123@gmail.com>
2022-04-09 22:19:09 -0400

    fixed gtag

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,22 @@ import React, { Fragment } from "react";
 import Router from "next/router";
 import { wrapper } from "../store";
 import { CookiesProvider } from "react-cookie";
-import Head from "next/head";
 import Script from "next/script";
+
 // global styles
 import "swiper/swiper.scss";
 import "rc-slider/assets/index.css";
 import "react-rater/lib/react-rater.css";
 import "../assets/css/styles.scss";
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-3QYSYY399Z"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-3QYSYY399Z');
+// </script>
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -22,17 +31,21 @@ const MyApp = ({ Component, pageProps }) => (
 	<CookiesProvider>
 		<Fragment>
 			{isProduction ? (
-				<Head>
+				<>
 					<Script
-						async
-						src="https://www.googletagmanager.com/gtag/js?id=G-3QYSYY399Z"
-					></Script>
-					<Script>
-						window.dataLayer = window.dataLayer || []; function
-						gtag(){dataLayer.push(arguments)}
-						gtag('js', new Date()); gtag('config', 'G-3QYSYY399Z');
+						strategy="lazyOnload"
+						src={
+							"https://www.googletagmanager.com/gtag/js?id=G-3QYSYY399Z"
+						}
+					/>
+					<Script strategy="lazyOnload">
+						{`window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-3QYSYY399Z');`}
 					</Script>
-				</Head>
+				</>
 			) : null}
 			<Component {...pageProps} />
 		</Fragment>
