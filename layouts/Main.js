b54badc4ece3e06187d247adef6a093d5fbda9c3
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-04-17 16:10:42 -0400

    Update Main.js

--- layouts/Main.js
+++ layouts/Main.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import { useRouter } from "next/router";
 
-export default ({ children, title = "DNC Nutrition" }) => {
+export default ({ children, title = "Discount Nutrition" }) => {
 	const router = useRouter();
 	const pathname = router.pathname;
 
