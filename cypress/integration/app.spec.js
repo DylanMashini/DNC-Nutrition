a9b61adbd660087e6d99b03a6329190dc0ad382f
DylanMashini <dylanmashini123@gmail.com>
2022-04-16 23:40:51 -0400

    updated tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -16,9 +16,12 @@ describe("Pagination", () => {
 		cy.visit("http://localhost:3000/products/55");
 		cy.get(
 			"#Pagination > nav > button.nextui-c-cTdvQQ.nextui-c-huiNHE.nextui-c-cTdvQQ-dZWCtT-active-true.nextui-c-cTdvQQ-SWDEj-animated-true.nextui-pagination-item.nextui-pagination-item-active.nextui-pagination-item-animated > span"
-		).should("contain", "55");
+		)
+			.should("contain", "55")
+			.end();
 	});
 });
+
 describe("Categories", () => {
 	it("should navigate to curamed catagories", () => {
 		cy.visit("http://localhost:3000/");
@@ -28,6 +31,54 @@ describe("Categories", () => {
 		cy.wait(1000);
 		cy.get(
 			"#__next > div > main > section.products-page > div > section > section > div:nth-child(1) > div.product__description > h3"
-		).contains("cura", { matchCase: false });
+		)
+			.contains("cura", { matchCase: false })
+			.end();
+	});
+});
+
+describe("Search", () => {
+	it("should search for anxiocalm", () => {
+		cy.visit("http://localhost:3000/");
+		cy.get(
+			"#__next > div > header > div > div > button.search-form-wrapper > i"
+		).click();
+		cy.get(
+			"#__next > div > header > div > div > button.search-form-wrapper.search-form--active > form > input[type=text]"
+		).type("anxiocalm{enter}");
+		cy.get(
+			"#__next > div > main > section.products-page > div > section > section > div:nth-child(1) > div.product__description > h3"
+		)
+			.contains("anxio", { matchCase: false })
+			.end();
+	});
+	it("should search for ion gut health", () => {
+		cy.visit("http://localhost:3000/");
+		cy.get(
+			"#__next > div > header > div > div > button.search-form-wrapper > i"
+		).click();
+		cy.get(
+			"#__next > div > header > div > div > button.search-form-wrapper.search-form--active > form > input[type=text]"
+		).type("ion{enter}");
+		cy.get(
+			"#__next > div > main > section.products-page > div > section > section > div:nth-child(1) > div.product__description > h3"
+		)
+			.contains("ion", { matchCase: false })
+			.end();
+	});
+});
+
+describe("Product Page", () => {
+	it("should select the first product and add it to the cart", () => {
+		cy.visit("http://localhost:3000");
+		cy.get(
+			"#__next > div > main > section.products-page > div > section > section > div:nth-child(1) > div.product__image > a > span > img"
+		).click();
+		cy.get(
+			"#__next > div > main > section.product-single > div > div.product-single__content > section.product-content > div.product-content__filters > div:nth-child(3) > div > button"
+		).click();
+		cy.get(
+			"#__next > div > header > div > div > button.btn-cart > span"
+		).should("be.visible");
 	});
 });
