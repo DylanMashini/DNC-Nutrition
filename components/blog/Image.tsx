2c63b37dcba5db9000af42a88001ed54bd7e2631
DylanMashini <dylanmashini123@gmail.com>
2022-07-20 14:47:13 -0400

    remove unnececary files from gh-pages branch

--- components/blog/Image.tsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image';
-export default function Named({src}) {
-    return(
-        <div id={"Image"}>
-            <Image src={src} object-fit="contain"/>
-        </div>
-    )
-}
--- pages/api/products.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-	const products = await fetch(
-		"https://dylanmashini.github.io/DNANutrition/prods.json"
-	).then(res => res.json());
-	res.status(200).json(products);
-}
