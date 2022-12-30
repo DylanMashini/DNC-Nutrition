8d35365d1084dfe39155328f4cd0eb253a5400f3
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-12-30 13:26:42 -0500

    fix: switch to production clover enviorment

--- src/utils/getAllProds.js
+++ src/utils/getAllProds.js
@@ -935,9 +935,9 @@ const run = (apiKey, merchantID, url) => {
 	}
 };
 run(
-	"f4e689f5-197b-3a25-9f99-0024da952d4f",
-	"ZX3HTEDW8YD01",
-	"https://sandbox.dev.clover.com"
+	"6df2d05b-0d13-900b-d245-db1367f4414b",
+	"DMF44Z2ZC6PTT",
+	"https://clover.com/"
 );
 //prod: 6df2d05b-0d13-900b-d245-db1367f4414b
 //prod: DMF44Z2ZC6PTT
