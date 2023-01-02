554ee7e6ec397fb582a8d02cce3b011e0dcbc976
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2023-01-02 14:25:35 -0500

    Fix: Fix server bug

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
-const test_env = process.env.TEST_ENV || (window && window.Cypress);
+const test_env =
+	process.env.TEST_ENV || (typeof window !== "undefined" && window.Cypress);
 
 export const server =
 	dev || test_env ? "http://localhost:3000" : "https://www.dncnutrition.com";
