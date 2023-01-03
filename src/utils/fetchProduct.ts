2cc8c32f7a278fcee9c9babd486c4a0062714409
DylanMashini <dylanmashini123@gmail.com>
2023-01-03 17:47:59 -0500

    Test Cache

--- src/utils/fetchProduct.ts
+++ src/utils/fetchProduct.ts
@@ -1,6 +1,6 @@
 import fetchProducts from "./fetchProducts";
 
-export default async function (id: string) {
+export default async function fetchProduct(id: string) {
 	const products = await fetchProducts();
 	const product = products.find(x => x.sku == 0 + id || x.sku == id);
 	return product;
